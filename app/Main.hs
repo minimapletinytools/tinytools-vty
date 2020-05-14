5a9beb111d568ff32eaec22cd0bb9ba91bda121e
pdlla <chippermonky@gmail.com>
2020-05-14 09:14:57 -0700

    added stderr logging to file

--- app/Main.hs
+++ app/Main.hs
@@ -5,14 +5,25 @@ module Main (
 ) where
 
 import           Prelude
+import           Relude
 
 import           Example
 import           Flow
 import           Potato
+
+import           GHC.IO.Handle
+import           GHC.IO.Handle.FD
+import           System.IO
 
 
 
 main :: IO ()
 --main = exampleMain
 --main = potatoMain
-main = flowMain
+main = do
+  -- vty takes over stdout so this only will work with stderr
+  fd <- openFile "stderr.txt" WriteMode
+  hDuplicateTo fd stderr  -- redirect stdout to file
+  hPutStrLn stderr "STDERR" -- will print to stderr
+  flowMain
+  hClose fd
