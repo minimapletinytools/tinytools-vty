477de994d610759b6dc7c16f2e8e49e943545c21
pdlla <chippermonky@gmail.com>
2020-05-15 13:13:40 -0700

    fixes to TrackedDrag stuff

--- app/Potato/Flow/Reflex/Vty/Canvas/Types.hs
+++ app/Potato/Flow/Reflex/Vty/Canvas/Types.hs
@@ -3,11 +3,17 @@
 
 
 module Potato.Flow.Reflex.Vty.Canvas.Types (
-  CanvasDrag
-  , TrackedDrag(..)
+  -- * generic stuff, prob should move into Potato.Reflex.Vty.Widget
+  TrackedDrag(..)
   , trackDrag
+  , neverTrackedDrag
   , captureTrackedDrag
-  , switchTrackedDrag
+  , alignTrackedDrag
+
+  -- * canvas specific stuff
+  , CanvasDrag
+  , CanvasTrackedDrag
+  , filterCanvasTrackedDrag
 ) where
 
 import           Relude
@@ -15,6 +21,7 @@ import           Relude
 import           Potato.Flow.Reflex.Vty.Tools
 import           Potato.Reflex.Vty.Widget
 
+import           Control.Lens                 (over, _1)
 import           Control.Monad.Fix
 import           Data.Semialign
 import           Data.These
@@ -25,14 +32,19 @@ import           Reflex
 import           Reflex.Potato.Helpers
 import           Reflex.Vty
 
-type CanvasDrag = ((Tool, (Int,Int)), Drag2)
 
 data TrackedDrag t a = TrackedDrag {
-  _trackedDrag_drag       :: Event t (a, Drag2) -- (tool at start of drag, pan position at start of drag, drag info)
+  -- | (user provided value, pan position at start of drag, drag info)
+  -- the use provided value is most likely something produced by drag2AttachOnStart but could be anything
+  _trackedDrag_drag       :: Event t (a, Drag2)
   , _trackedDrag_cancel   :: Event t ()
   , _trackedDrag_dragging :: Dynamic t Bool
 }
 
+instance (Reflex t) => Functor (TrackedDrag t) where
+  fmap f td = td { _trackedDrag_drag = fmap (over _1 f) $ _trackedDrag_drag td }
+
+-- NOTE should not be terrible if we every want to switch to Dynamic t (Dynamic t (a, Drag2)), but I think this way is simpler
 trackDrag :: (Reflex t, MonadFix m, MonadHold t m)
   => Event t (a, Drag2)
   -> Event t () -- ^ force cancel event
@@ -52,18 +64,20 @@ trackDrag dragEv cancelEv = do
       , _trackedDrag_dragging = isTrackingDyn
     }
 
-captureTrackedDrag :: forall t m a. (Reflex t, MonadFix m, MonadHold t m)
+neverTrackedDrag :: (Reflex t) => TrackedDrag t a
+neverTrackedDrag = TrackedDrag {
+    _trackedDrag_drag = never
+    , _trackedDrag_cancel = never
+    , _trackedDrag_dragging = constDyn False
+  }
+
+-- | chain these together to ensure each drag has only one consumer
+captureTrackedDrag' :: forall t m a. (Reflex t, MonadFix m, MonadHold t m)
   => TrackedDrag t a
   -> Event t () -- ^ captures the whole drag if this event fires at the same time as a DragStart
-  -> m (Dynamic t (TrackedDrag t a))
-captureTrackedDrag orig@TrackedDrag {..} didCaptureEv = do
+  -> m (Dynamic t (TrackedDrag t a, TrackedDrag t a)) -- ^ (uncaptured, captured)
+captureTrackedDrag' orig@TrackedDrag {..} didCaptureEv = do
   let
-    neverPass = TrackedDrag {
-        _trackedDrag_drag = never
-        , _trackedDrag_cancel = never
-        , _trackedDrag_dragging = constDyn False
-      }
-
     -- TODO rewrite this to use _trackedDrag_dragging
     isCapturingDyn_foldfn :: These (Either DragState ()) () -> Bool -> Maybe Bool
     -- begin capture on simultaneous DragStart and capture events
@@ -77,9 +91,9 @@ captureTrackedDrag orig@TrackedDrag {..} didCaptureEv = do
     isCapturingDyn_foldfn _ _                          = Nothing
 
   isCapturingDyn <- foldDynMaybe isCapturingDyn_foldfn False $ align (alignEitherAssert "capture drag" (fmap (_drag2_state . snd) _trackedDrag_drag) _trackedDrag_cancel) didCaptureEv
-  return $ fmap (\c -> if c then neverPass else orig) isCapturingDyn
+  return $ fmap (\c -> if c then (neverTrackedDrag, orig) else (orig, neverTrackedDrag)) isCapturingDyn
 
-switchTrackedDrag :: (Reflex t) => Dynamic t (TrackedDrag t a ) -> TrackedDrag t a
+switchTrackedDrag :: (Reflex t) => Dynamic t (TrackedDrag t a) -> TrackedDrag t a
 switchTrackedDrag dtd = r where
   dragEv = switchDyn $ fmap _trackedDrag_drag dtd
   cancelEv = switchDyn $ fmap _trackedDrag_cancel dtd
@@ -89,3 +103,35 @@ switchTrackedDrag dtd = r where
       , _trackedDrag_cancel = cancelEv
       , _trackedDrag_dragging = draggingBeh
     }
+
+captureTrackedDrag :: forall t m a. (Reflex t, MonadFix m, MonadHold t m)
+  => TrackedDrag t a
+  -> Event t () -- ^ captures the whole drag if this event fires at the same time as a DragStart
+  -> m (TrackedDrag t a)
+captureTrackedDrag orig didCaptureEv = do
+  tddyn <- captureTrackedDrag' orig didCaptureEv
+  return $ switchTrackedDrag (fmap fst tddyn)
+
+alignTrackedDrag :: (Reflex t)
+  => TrackedDrag t a
+  -> Event t (Either (a ,Drag2) ())
+alignTrackedDrag TrackedDrag {..} = alignEitherAssert "alignTrackedDrag" _trackedDrag_drag _trackedDrag_cancel
+
+
+-- canvas specific stuff
+type CanvasDrag = ((Tool, (Int,Int)), Drag2)
+type CanvasTrackedDrag t = TrackedDrag t (Tool, (Int,Int))
+
+-- potato filter for tracked drag events, does not pl
+filterCanvasTrackedDrag :: forall t m. (Reflex t, MonadFix m, MonadHold t m)
+  => Maybe Tool -- ^ tool state to select for
+  -> CanvasTrackedDrag t
+  -> m (CanvasTrackedDrag t , TrackedDrag t (Int,Int)) -- ^ (drags that were not caught by filter, filtered drag with Tool removed)
+filterCanvasTrackedDrag c' trackedDrag = do
+  let
+    captureEv = fmapMaybe (\((c,_),_) -> if maybe True (c ==) c' then Just () else Nothing) (_trackedDrag_drag trackedDrag)
+  tddyn <- captureTrackedDrag' trackedDrag captureEv
+  let
+    uncaptured = switchTrackedDrag (fmap fst tddyn)
+    captured = switchTrackedDrag (fmap (fmap snd . snd) tddyn)
+  return (uncaptured, captured)
