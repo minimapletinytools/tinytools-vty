d40740c27d738882af85a0f74a5c2e83df6fc2bd
pdlla <chippermonky@gmail.com>
2020-05-23 13:03:36 -0700

    reflex vty test host basic sketch done

--- src/Reflex/Vty/Test/Monad/Host.hs
+++ src/Reflex/Vty/Test/Monad/Host.hs
@@ -7,14 +7,15 @@ where
 
 import           Relude
 
-import Control.Monad.Ref
-import qualified Data.List.NonEmpty as NE
+import           Control.Monad.Ref
+import qualified Data.List.NonEmpty     as NE
 
+import qualified Graphics.Vty           as V
 import           Reflex
 import           Reflex.Host.Class
-import           Reflex.Test.Monad.Host
+import  Reflex.Test.Monad.Host (MonadReflexTest(..), ReflexTestT, ReflexTriggerRef, TestGuestT, TestGuestConstraints, runReflexTestT)
 import           Reflex.Vty
-import qualified Graphics.Vty as V
+import Reflex.Spider.Internal (HasSpiderTimeline)
 
 
 type ReflexVtyTestT t uintref uout m = ReflexTestT t (uintref, ReflexTriggerRef t m VtyEvent) (uout, Behavior t [V.Image]) m
@@ -50,50 +51,52 @@ vtyOutputs = do
   return vtyoutputs
 
 -- | if (local) mouse coordinates are outside of the (absolute) region, returns False and does not queue any event
-queueMouseEventInRegion :: (Reflex t, MonadSample t m, Monad m, MonadRef m) 
-  => DynRegion t 
+queueMouseEventInRegion :: (Reflex t, MonadSample t m, Monad m, MonadRef m)
+  => DynRegion t
   -> Either MouseDown MouseUp -- ^ mouse coordinates are LOCAL to the input region
   -> ReflexVtyTestT t uintref uout m Bool
 queueMouseEventInRegion dr mouse = do
   region <- sample . currentRegion $ dr
   let
     absCoords (Region l t _ _) (x,y) = (x+l, y+t)
-    coordinates = case mouse of 
+    coordinates = case mouse of
       Left (MouseDown _ c _) -> c
-      Right (MouseUp _ c) -> c
+      Right (MouseUp _ c)    -> c
     withinRegion (Region _ _ w h) (x,y) = not $ or [ x < 0, y < 0, x >= w, y >= h ]
     --withinRegion (Region l t w h) (x,y) = not $ or [ x < l, y < t, x >= l + w, y >= t + h ]
-  
+
   if withinRegion region coordinates
     then do
-      case mouse of 
+      case mouse of
         Left (MouseDown b c mods) -> queueVtyEvent $ uncurry V.EvMouseDown (absCoords region c) b mods
         Right (MouseUp b c) -> queueVtyEvent $ uncurry V.EvMouseUp (absCoords region c) b
       return True
     else return False
 
 -- | queue and fire a series of mouse events representing a mouse drag
--- returns collected outputs 
-queueMouseDrag :: (Monad m, MonadRef m) 
+-- returns collected outputs
+queueMouseDrag :: (Monad m, MonadRef m)
   => V.Button -- ^ button to press
   -> [V.Modifier] -- ^ modifier held during drag
   -> NonEmpty (Int,Int) -- ^ list of drag positions
   -- TODO add something like DragState to this
-  -> ((Int,Int) -> ReadPhase m a) -- ^ ReadPhase to run after each normal drag 
+  -> ((Int,Int) -> ReadPhase m a) -- ^ ReadPhase to run after each normal drag
   -> ReflexVtyTestT t uintref uout m (NonEmpty [a]) -- ^ collected outputs
 queueMouseDrag b mods ps rps = do
   let
     dragPs' = init ps
     -- if there is only 1 elt in ps, then simulate a single click
-    dragPs = fromMaybe (pure (head ps)) $ viaNonEmpty id dragPs'  
+    dragPs = fromMaybe (pure (head ps)) $ viaNonEmpty id dragPs'
     endP = last ps
   initas <- forM dragPs $ \p -> do
-    queueVtyEvent (uncurry V.EvMouseDown p b mods) 
+    queueVtyEvent (uncurry V.EvMouseDown p b mods)
     fireQueuedEventsAndRead (rps p)
   queueVtyEvent (uncurry V.EvMouseUp endP (Just b))
   lastas <- fireQueuedEventsAndRead (rps endP)
   return $ initas <> (lastas :| [])
 
+-- | run a 'ReflexVtyTestT'
+-- analogous to runReflexTestT
 runReflexVtyTestT :: forall uintref uinev uout t m a. (MonadVtyApp t (TestGuestT t m), TestGuestConstraints t m)
   => Dynamic t Int -- ^ virtual screen width
   -> Dynamic t Int -- ^ virtual screen height
@@ -107,7 +110,7 @@ runReflexVtyTestT dw dh (uinput, uinputtrefs) app rtm = do
   (vinev, vintref) <- newEventWithTriggerRef
 
   -- pass it on as ctx object
-  let ctx = VtyWidgetCtx { 
+  let ctx = VtyWidgetCtx {
       _vtyWidgetCtx_width = dw
       , _vtyWidgetCtx_height = dh
       , _vtyWidgetCtx_input = vinev
@@ -116,30 +119,28 @@ runReflexVtyTestT dw dh (uinput, uinputtrefs) app rtm = do
 
   -- unwrap VtyWidget and pass to runReflexTestT
   runReflexTestT
-    ((uinput, vinev), (uinputtrefs, vintref)) 
+    ((uinput, vinev), (uinputtrefs, vintref))
     (\(uinput',_) -> runNodeIdT $ runVtyWidget ctx (app uinput'))
     rtm
-  
 
 
-{-
--- | class to help bind network and types to a 'ReflexTestT'
--- see test/Reflex/Test/Monad/HostSpec.hs for usage example
+-- | class to help bind network and types to a 'ReflexVtyTestT'
+-- analogous to ReflexTestApp
 class ReflexVtyTestApp app t m | app -> t m where
   data VtyAppInputTriggerRefs app :: Type
   data VtyAppInputEvents app :: Type
   data VtyAppOutput app :: Type
-  getApp :: VtyAppInputEvents app -> TestGuestT t m (VtyWidget t m (AppOutput app))
+  getApp :: VtyAppInputEvents app -> VtyWidget t (NodeIdT (TestGuestT t m)) (VtyAppOutput app)
   makeInputs :: m (VtyAppInputEvents app, VtyAppInputTriggerRefs app)
 
-instance (ReflexVtyTestApp app t m) => ReflexTestApp app t m where
-  data AppInputTriggerRefs app = VtyAppInputTriggerRefs app
-  data AppInputEvents app :: Type
-  data AppOutput app :: Type
-  getApp :: AppInputEvents app -> TestGuestT t m (AppOutput app)
-  makeInputs :: m (AppInputEvents app, AppInputTriggerRefs app)
--}
-
+runReflexVtyTestApp :: (ReflexVtyTestApp app t m, MonadVtyApp t (TestGuestT t m), TestGuestConstraints t m)
+  => Dynamic t Int -- ^ virtual screen width
+  -> Dynamic t Int -- ^ virtual screen height
+  -> ReflexVtyTestT t (VtyAppInputTriggerRefs app) (VtyAppOutput app) m ()
+  -> m ()
+runReflexVtyTestApp dw dh rtm = do
+  input <- makeInputs
+  runReflexVtyTestT dw dh input getApp rtm
 
 
 
