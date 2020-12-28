1736ba951a03c60b043b04f6e532d7bbf8e203fa
pdlla <chippermonky@gmail.com>
2020-12-27 19:21:06 -0800

    added a bunch of input helper stuff

--- app/Flow.hs
+++ app/Flow.hs
@@ -22,12 +22,108 @@ import           Potato.Reflex.Vty.Widget
 import           Control.Monad.Fix
 import           Control.Monad.NodeId
 import qualified Data.Aeson                         as Aeson
+import qualified Data.Text.Encoding                 as T
 import           Data.Time.Clock
 
 import qualified Graphics.Vty                       as V
+import qualified Graphics.Vty.Input.Events          as V
 import           Reflex
 import           Reflex.Potato.Helpers
 import           Reflex.Vty
+
+
+
+
+
+convertModifiers :: [V.Modifier] -> [KeyModifier]
+convertModifiers = fmap $ \case
+  V.MShift -> KeyModifier_Shift
+  V.MCtrl  -> KeyModifier_Ctrl
+  V.MMeta  -> KeyModifier_Ctrl
+  V.MAlt ->  KeyModifier_Alt
+
+convertKey :: V.Key -> Maybe KeyboardKey
+convertKey = \case
+  V.KEsc   -> Just KeyboardKey_Esc
+  V.KChar c -> Just $   KeyboardKey_Char c
+  V.KBS   -> Just KeyboardKey_Space
+  V.KEnter   -> Just KeyboardKey_Return
+  V.KLeft   -> Just KeyboardKey_Left
+  V.KRight   -> Just KeyboardKey_Right
+  V.KUp   -> Just KeyboardKey_Up
+  V.KDown   -> Just KeyboardKey_Down
+  V.KUpLeft   -> Nothing
+  V.KUpRight -> Nothing
+  V.KDownLeft -> Nothing
+  V.KDownRight -> Nothing
+  V.KCenter -> Nothing
+  V.KFun _ -> Nothing
+  V.KBackTab -> Nothing
+  V.KPrtScr -> Nothing
+  V.KPause -> Nothing
+  V.KIns -> Nothing
+  V.KHome -> Just KeyboardKey_Home
+  V.KPageUp -> Nothing
+  V.KDel -> Just KeyboardKey_Delete
+  V.KEnd -> Just KeyboardKey_End
+  V.KPageDown -> Nothing
+  V.KBegin -> Nothing
+  V.KMenu -> Nothing
+
+
+convertButton :: V.Button -> Maybe MouseButton
+convertButton = \case
+  V.BLeft -> Just MouseButton_Left
+  V.BMiddle -> Just MouseButton_Middle
+  V.BRight -> Just MouseButton_Right
+  V.BScrollUp -> Nothing
+  V.BScrollDown -> Nothing
+
+makeLMouseDataInputEv
+  :: (Reflex t, MonadFix m, MonadHold t m)
+  => XY
+  -> Bool
+  -> VtyWidget t m (Event t LMouseData)
+makeLMouseDataInputEv offset isLayerMouse = mdo
+  -- NOTE, must report both mouse down and up for any given drag or things will break
+  -- button/mods is always the same button as mouse down, even if it changes during a drag
+  inp <- input
+
+  let
+    mouseDownEv = fforMaybe inp $ \case
+      V.EvMouseDown _ _ b mods -> Just (b, mods)
+      _ -> Nothing
+    -- tracks if last event was a mouse up
+    mouseUpEv = fforMaybe inp $ \case
+      V.EvMouseUp _ _ _ -> Just True
+      V.EvMouseDown _ _ _ _ -> Just False
+      _ -> Nothing
+    mouseDownFoldFn (True, x) _  = x -- only updated button/mods just after a mouse up
+    mouseDownFoldFn (False, _) x = x
+  mouseUpDyn <- holdDyn True mouseUpEv
+  mouseDownDyn <- foldDyn mouseDownFoldFn (V.BLeft,[]) (attach (current mouseUpDyn) mouseDownEv)
+
+  return $ fforMaybe (attach (current mouseDownDyn) inp) $ \case
+    (_, V.EvMouseDown _ _ V.BScrollUp _) -> Nothing
+    (_, V.EvMouseDown _ _ V.BScrollDown _) -> Nothing
+    (_, V.EvMouseDown x y b mods) -> case convertButton b of
+      Nothing -> Nothing
+      Just b' -> Just $ LMouseData {
+        _lMouseData_position       = (V2 x y) + offset
+        , _lMouseData_isRelease    = False
+        , _lMouseData_button       = b'
+        , _lMouseData_modifiers    = convertModifiers mods
+        , _lMouseData_isLayerMouse = isLayerMouse
+      }
+    ((b,mods), V.EvMouseUp x y _) -> case convertButton b of
+      Nothing -> Nothing
+      Just b' -> Just $ LMouseData {
+        _lMouseData_position       = (V2 x y) + offset
+        , _lMouseData_isRelease    = True
+        , _lMouseData_button       = b'
+        , _lMouseData_modifiers    = convertModifiers mods
+        , _lMouseData_isLayerMouse = isLayerMouse
+      }
 
 
 flowMain :: IO ()
@@ -39,6 +135,18 @@ data MainPFTestOutput t = MainPFTestOutput {
   _mainPFTestOutput_leftPane    :: DynRegion t
   , _mainPFTestOutput_rightPane :: DynRegion t
 }
+
+{-
+verifyInput :: (Reflex t, MonadHold t m) => Event t VtyEvent -> m (Event t VtyEvent)
+verifyInput ev = do
+  let
+    foldDynMaybeFn = \case
+      EvMouseDown _ _ _ _ -> Just True
+      EvMouseUp _ _ _ -> Just False
+      _ -> Nothing
+  isMouseDownDyn <- foldDynMaybe foldDynMaybeFn False ev
+  -- TODO check for invalid key presses based on mouse state
+-}
 
 
 mainPFWidget :: forall t m. (Reflex t, MonadHold t m, MonadFix m, NotReady t m, Adjustable t m, PostBuild t m, PerformEvent t m, TriggerEvent t m, MonadNodeId m, MonadIO (Performable m), MonadIO m)
@@ -60,19 +168,10 @@ mainPFWidget = mdo
         , _pFWidgetCtx_initialPFState = pfstate_basic1
       }
 
-    -- TODO DELETE prob just let goat handle this
-    undoEv = fforMaybe inp $ \case
-      V.EvKey (V.KChar 'z') [V.MCtrl] -> Just ()
+    keyboardEv = fforMaybe inp $ \case
+      V.EvKey k mods -> convertKey k >>= (\kbd -> return $ KeyboardData kbd (convertModifiers mods))
+      V.EvPaste bs -> Just $ KeyboardData (KeyboardKey_Paste (T.decodeUtf8 bs)) []
       _ -> Nothing
-    redoEv = fforMaybe inp $ \case
-      V.EvKey (V.KChar 'y') [V.MCtrl] -> Just ()
-      _ -> Nothing
-    saveEv = fforMaybe inp $ \case
-      V.EvKey (V.KChar 's') [V.MCtrl] -> Just ()
-      _ -> Nothing
-
-    -- TODO setup keyboard input
-    -- I guess we need to pass to params and popups first before passing to controller? idk
 
 
 
