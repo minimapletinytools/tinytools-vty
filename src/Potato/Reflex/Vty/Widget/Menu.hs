adbfc1d209f1a48dbdcfecd44f80545a0e101958
pdlla <chippermonky@gmail.com>
2021-02-07 14:01:52 -0800

    wip menu stuff, probbaly start over using zippers

--- src/Potato/Reflex/Vty/Widget/Menu.hs
+++ src/Potato/Reflex/Vty/Widget/Menu.hs
@@ -14,9 +14,15 @@ import qualified Graphics.Vty.Input.Events as V
 import           Reflex
 import           Reflex.Network
 import           Reflex.Potato.Helpers
-import           Reflex.Vty
+import           Potato.Reflex.Vty.Widget.Layout
+import           Reflex.Vty                        hiding (Constraint (..),
+                                                    Orientation (..), col,
+                                                    fixed, row, stretch, tile, TileConfig(..), Layout(..))
+
 
 import           Data.Default
+import qualified Data.Map as Map
+import qualified Data.Text as T
 import Data.Tree
 
 type Shortcut = ()
@@ -24,39 +30,81 @@ type Shortcut = ()
 data MenuTreeNode k = MenuTreeNode {
   _menuTreeNode_key :: k -- MUST BE UNIQUE
   , _menuTreeNode_label :: Text
-  , _menuTreeNode_shortcut :: Shortcut
+  , _menuTreeNode_shortcut :: Maybe Shortcut
 } deriving (Eq, Show)
 
 data MenuWidgetConfig t k = MenuWidgetConfig {
-  _menuWidgetConfig_menus :: Dynamic t (Tree (MenuTreeNode k))
+  _menuWidgetConfig_menus :: Dynamic t ([Tree (MenuTreeNode k)])
 }
 
 data MenuWidget t k = MenuWidget {
-  _menuWidget_selected :: Event t k
+  _menuWidget_activated :: Event t k
   , _menuWidget_capturedInput :: Event t ()
 }
 
+indexedMerge :: (Reflex t) => [Event t a] -> Event t (Int,a)
+indexedMerge evs = leftmost . fmap (\(i,ev)-> fmap (i,) ev) . zip [0..] $ evs
+
+type Pos = (Int,Int)
+
+type MenuFocus k = [(k, Int, Pos)]
+
+holdMenuWidget = undefined
+
+{-
 holdMenuWidget ::
-  (MonadWidget t m)
+  forall t m k. (MonadWidget t m)
   => MenuWidgetConfig t k
   -> VtyWidget t m (MenuWidget t k)
 holdMenuWidget MenuWidgetConfig {..} = mdo
+  let
+    makeMenuWidget :: [Tree (MenuTreeNode k)] -> VtyWidget t m ()
+    makeMenuWidget roots = do
+      -- TODO
+      -- if line down to currently opened menu still exists, keep focus (I guess to do this really right, you should track index as well so there's no menu jump glitcehs)
 
-  -- on menu change
-    -- if line down to currently opened menu still exists, keep focus (I guess to do this really right, you should track index as well so there's no menu jump glitcehs)
-    -- otherwise rerender the whole darn menu and lose focus
+      -- setup shortcut map
+      let
+        shortcutfmapfn MenuTreeNode {..} = case _menuTreeNode_shortcut of
+          Nothing -> Nothing
+          Just x -> Just (x, _menuTreeNode_key)
+        rootMaybeShortcuts = fmap (fmap shortcutfmapfn) roots
+        shortcuts = catMaybes . join . fmap flatten $ rootMaybeShortcuts
+        shortcutMap :: Behavior t (Map.Map Shortcut k) = constant $ Map.fromList $ shortcuts
 
-    -- shortcutMap :: Behavior t (Map Shortcut k)
+      -- render the first layer at top row
+      topLayerOut <- beginLayout $ row $
+        forM roots $ \(Node MenuTreeNode {..} _) -> do
+          let l = T.length _menuTreeNode_label
+          fixed (constDyn $ l + 1) $ do
+            -- TODO highlight if selected in focusDyn
+            text (constant _menuTreeNode_label)
+            click <- mouseDown V.BLeft
+            return (l+1, click $> _menuTreeNode_key)
+      let
+        topOffsets = scanl (+) 0 $ fmap fst topLayerOut
+        -- (index, xoffset)
+        topEv :: Event t (Int, Int) = indexedMerge $ zipWith (\offx ev -> fmap (,offx) ev) topOffsets $ fmap snd topLayerOut
 
-  -- focusDyn :: Dynamic t ([(k, Int, Pos)])
+        rootSetFocusEv = ffor topEv $ \(idx,(k,xoff)) -> [(k,idx,(xoff,1))]
 
-  -- render first layer at top of screen
-    -- render each following layer
+        childWidgetFn [] = return never
+        childWidgetFn ((k,idx,(xoff,yoff)):xs) = do
+          forM
+          selfClickEv
+          childClickEv <- childWidgetFn xs
+          return $ leftmost [selfClickEv, childClickEv]
+
+      -- TODO list to changes in focus Dyn and render each following layer
+      childMenusDyn = ffor (fmap tail focusDyn) $ childWidgetFn
 
 
+      focusDyn :: Dynamic t (MenuFocus k) <- undefined
+      return ()
 
 
   return MenuWidget {
-      _menuWidget_selected = never
+      _menuWidget_activated = never
       , _menuWidget_capturedInput = never
     }
+-}
