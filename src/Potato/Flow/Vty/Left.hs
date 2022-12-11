449090cf14e0df20981f5e978902ca3b542897c1
pdlla <chippermonky@gmail.com>
2022-12-11 12:07:32 -0800

    cleanup in Left

--- src/Potato/Flow/Vty/Left.hs
+++ src/Potato/Flow/Vty/Left.hs
@@ -58,8 +58,11 @@ holdLeftWidget :: forall t m. (MonadWidget t m, HasPotato t m)
 holdLeftWidget LeftWidgetConfig {..} = do
 
   widthDyn <- displayWidth
+  -- this will toggle true/fales as you click between left/canvas panels
+  -- this only works because you use splitHDrag to split the Left/Canvas panels (it toggles the focus)
   focusDyn <- focus
   let
+
     loseFocusEv = void $ ffilter (not . id) $ updated focusDyn
 
   initLayout $ col $ mdo
@@ -67,33 +70,30 @@ holdLeftWidget LeftWidgetConfig {..} = do
 
     -- TODO consider
     -- TODO height should be dynamic but not sure if there's away to do this dynamically because width (from which buttonsHeightDyn) is derived depends on `grout . fixed`. You need to pull width from outside of the `grout . fixed` call to make this work right...
-    (clickSaveEv_d1, clickSaveAsEv_d1, exportEv_d1, quitEv_d1, buttonsHeightDyn) <- (grout . fixed) buttonsHeightDyn $ row $ do
+    (menuButtons, buttonsHeightDyn) <- (grout . fixed) buttonsHeightDyn $ row $ do
 
       (buttonsEv, heightDyn) <- buttonList (constDyn ["save", "save as", "export to \"potato.txt\"", "quit"]) (Just widthDyn)
       let
-        clickSaveEv_d2 = ffilterButtonIndex 0 buttonsEv
-        clickSaveAsEv_d2 = ffilterButtonIndex 1 buttonsEv
-        exportEv_d2 = ffilterButtonIndex 2 buttonsEv
-        quitEv_d2 = ffilterButtonIndex 3 buttonsEv
-        clickPrintEv = tag (current $ _goatWidget_renderedCanvas _layersWidgetConfig_goatW) (void exportEv_d2)
+        exportEv = ffilterButtonIndex 2 buttonsEv
+        menuButtons' = MenuButtonsWidget {
+            _menuButtonsWidget_saveEv = ffilterButtonIndex 0 buttonsEv
+            , _menuButtonsWidget_saveAsEv = ffilterButtonIndex 1 buttonsEv
+            , _menuButtonsWidget_exportEv = exportEv
+            , _menuButtonsWidget_quitEv = ffilterButtonIndex 3 buttonsEv
+          }
+        clickPrintEv = tag (current $ _goatWidget_renderedCanvas _layersWidgetConfig_goatW) (void exportEv)
       -- TODO don't do this here cmon...
       performEvent_ $ ffor clickPrintEv $ \rc -> do
          let t = renderedCanvasToText rc
          -- TODO at least use filename...
          liftIO $ T.writeFile "potato.txt" t
-      return (clickSaveEv_d2, clickSaveAsEv_d2, exportEv_d2, quitEv_d2, heightDyn)
-    let
-      menuButtons = MenuButtonsWidget {
-          _menuButtonsWidget_saveEv = clickSaveEv_d1
-          , _menuButtonsWidget_saveAsEv = clickSaveAsEv_d1
-          , _menuButtonsWidget_exportEv = exportEv_d1
-          , _menuButtonsWidget_quitEv = quitEv_d1
-        }
+      return (menuButtons', heightDyn)
+
 
 
     hdivider
 
-    tools <- (grout . fixed) (_toolWidget_heightDyn tools) $ holdToolsWidget $  ToolWidgetConfig {
+    tools <- (grout . fixed) (_toolWidget_heightDyn tools) $ holdToolsWidget $ ToolWidgetConfig {
         _toolWidgetConfig_tool =  _goatWidget_tool _layersWidgetConfig_goatW
         , _toolWidgetConfig_widthDyn = widthDyn
       }
